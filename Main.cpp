de406e1131bfebd447a1f12cc31564c89e37bd69
Akashmed <akashmed3@gmail.com>
2025-02-24 12:36:09 +0600

    first development push

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 #include "User.h"
 
-int main(){
+int main()
+{
     Guest guest1("john_doe", "1234", "John Doe");
     guest1.displayInfo();
+
+    bool a = guest1.authenticate("john_doe", "1234");
+    cout << a << endl;
 
     return 0;
 }
