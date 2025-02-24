a833c9c2f94f393dd7ea74aef6ec662eb9db662b
Akashmed <akashmed3@gmail.com>
2025-02-24 14:48:17 +0600

    nothing changed

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,13 @@ int main()
 {
     Guest guest1("john_doe", "1234", "John Doe");
     guest1.displayInfo();
+     
+    string username;
+    string password;
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
 
     bool a = guest1.authenticate("john_doe", "1234");
     cout << a << endl;
