9df87c41c110e5c8536529e71e7674cfaf10a907
Akashmed <akashmed3@gmail.com>
2025-03-01 00:35:55 +0600

    modified authenticate function, added 3 chances to login

--- Main.cpp
+++ Main.cpp
@@ -4,18 +4,9 @@ using namespace std;
 
 int main()
 {
-    Guest guest1("john_doe", "1234", "John Doe");
-    guest1.displayInfo();
-     
-    string username;
-    string password;
-    cout<<"Enter username: ";
-    cin>>username;
-    cout<<"Enter password: ";
-    cin>>password;
-
-    bool a = guest1.authenticate("john_doe", "1234");
-    cout << a << endl;
-
+    Guest guest1;
+    guest1.createAccount();
+    cout<<"Please login to your account"<<endl;
+    guest1.authenticate();
     return 0;
 }
